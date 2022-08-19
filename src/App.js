2b4ec7414815d2f6fcbcece1ba6369a4cd29919b
Annie <anniehartmann97@gmail.com>
2022-08-20 02:03:38 +0300

    Add files

--- src/App.js
+++ src/App.js
@@ -1,84 +1,15 @@
-import { useState } from 'react';
+import React from 'react';
 import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
 
-const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
-
-function Result({ correct, setStep, setCorrect }) {
-  const resetProgress = () => {
-    setStep(0);
-    setCorrect(0);
-  };
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="img" />
-      <h2>
-        Вы отгадали {correct} ответа из {questions.length}
-      </h2>
-      <button onClick={resetProgress}>Попробовать снова</button>
-    </div>
-  );
-}
-
-function Game({ step, question, onClickVariant }) {
-  const { title, variants } = question;
-  const percentage = Math.round((step / questions.length) * 100);
-
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
-      </div>
-      <h1>{title}</h1>
-      <ul>
-        {variants.map((variant, index) => (
-          <li key={index} onClick={() => onClickVariant(index)}>
-            {variant}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
+// Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  const [step, setStep] = useState(0);
-  const [correct, setCorrect] = useState(0);
-  const question = questions[step];
-
-  const onClickVariant = (index) => {
-    setStep(step + 1);
-    if (index === question.correct) {
-      setCorrect(correct + 1);
-    }
-  };
-
   return (
     <div className="App">
-      {step !== questions.length ? (
-        <Game step={step} question={question} onClickVariant={onClickVariant} />
-      ) : (
-        <Result correct={correct} setStep={setStep} setCorrect={setCorrect} />
-      )}
+      <Users />
+      {/* <Success /> */}
     </div>
   );
 }
