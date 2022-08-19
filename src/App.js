2424a45488831f71536372b6e356a9eeb29ec12e
Annie <anniehartmann97@gmail.com>
2022-08-20 02:25:14 +0300

    Get data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,9 +6,20 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((err) => console.warn(err))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="App">
-      <Users />
+      <Users items={users} isLoading={isLoading} />
       {/* <Success /> */}
     </div>
   );
