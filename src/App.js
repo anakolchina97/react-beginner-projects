01f5a82b91ac5e2c02810b6cfd6f73f75c7f5c55
Annie <anniehartmann97@gmail.com>
2022-08-20 00:11:22 +0300

    :tada: Quiz

--- src/App.js
+++ src/App.js
@@ -1,17 +1,84 @@
-import React, { useState } from 'react';
-import Modal from './components/Modal';
+import { useState } from 'react';
 import './index.scss';
 
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+function Result({ correct, setStep, setCorrect }) {
+  const resetProgress = () => {
+    setStep(0);
+    setCorrect(0);
+  };
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="img" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <button onClick={resetProgress}>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const { title, variants } = question;
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
+      </div>
+      <h1>{title}</h1>
+      <ul>
+        {variants.map((variant, index) => (
+          <li key={index} onClick={() => onClickVariant(index)}>
+            {variant}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
 function App() {
-  const [openModal, setOpenModal] = useState(false);
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <button className="open-modal-btn" onClick={() => setOpenModal(true)}>
-        ✨ Открыть окно
-      </button>
-      <Modal openModal={openModal} setOpenModal={setOpenModal}>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="gif" />
-      </Modal>
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} setStep={setStep} setCorrect={setCorrect} />
+      )}
     </div>
   );
 }
